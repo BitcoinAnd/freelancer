7ed2278f6c6cf53f2ad00d8fd54bc8af3c242047
RandyMcMillan <randy.lee.mcmillan@gmail.com>
2020-10-26 14:10:48 -0400

    some config

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-    * Start Bootstrap - Freelancer v6.0.4 (https://startbootstrap.com/themes/freelancer)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+    * Start Bootstrap - BitcoinAnd.net v6.0.4 (https://bitcoinAnd.net)
+    * Copyright 2013-2020 BitcoinAnd.net
+    * Licensed under MIT (https://github.com/StartBootstrap/BitcoinAnd.net/blob/master/LICENSE)
     */
     (function($) {
     "use strict"; // Start of use strict
